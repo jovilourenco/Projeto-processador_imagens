a19a75339e8b152a1f3cc2f8d7ea6ddae9b892cd
Victor G. Menezes <victormrdv2@gmail.com>
2026-03-18 19:56:05 -0300

    fix: ajuste no sobel para editar imagens em escala de cinza

--- home/filters/bordas.py
+++ home/filters/bordas.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
+from .geral import gray_scale
 
 
 def sobel(img: np.ndarray, **params) -> np.ndarray:
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = img if len(img.shape) == 2 else gray_scale(img)
     sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3)
     sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)
     magnitude = cv2.magnitude(sobelx, sobely)
